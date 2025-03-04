916a74eb3ef20fa1f94c771e9c608a5308f0720a
safi miazi <mohibullamiazi@gmail.com>
2025-03-04 18:51:08 +0600

    add Sidebar and TopNav components; implement responsive mobile menu in Sidebar; update MainLayout layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,10 +1,21 @@
+import Sidebar from "../common/Sidebar";
+import TopNav from "../common/TopNav";
 
 const MainLayout = () => {
-    return (
-        <div className='flex'>
-            
-        </div>
-    );
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="w-full min-w-0 flex  flex-col flex-1 ">
+        <header className="h-16 border-b border-gray-200 ">
+          <TopNav />
+        </header>
+        <main className="flex-1 overflow-auto p-6 bg-white">
+          {/* Your app content goes here */}
+          Hello from MainLayout!
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
