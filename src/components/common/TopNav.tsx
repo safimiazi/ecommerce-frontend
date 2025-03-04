82e7682a2b6e7d03a697077a2f99516690066038
safi miazi <mohibullamiazi@gmail.com>
2025-03-04 18:54:04 +0600

    remove placeholder text from TopNav component

--- src/components/common/TopNav.tsx
+++ src/components/common/TopNav.tsx
@@ -2,7 +2,7 @@
 const TopNav = () => {
     return (
         <div className=" px-3  sm:px-6 flex items-center justify-between bg-white border-b border-gray-200 h-full">
-            top nav
+           
         </div>
     );
 };
