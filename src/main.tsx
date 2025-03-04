42b37cf6435b3d9d1a6f1e7b16c3ace298430390
safi miazi <mohibullamiazi@gmail.com>
2025-03-04 19:38:29 +0600

    add react-router-dom for routing; wrap App component with ConfigProvider for theme customization; create routes configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ConfigProvider } from "antd";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: "#00b96b",
+          borderRadius: 2,
+
+          // Alias Token
+          colorBgContainer: "#f6ffed",
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </StrictMode>
+);
