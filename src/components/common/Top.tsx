5a416ecb7368d1bf2bcb179fc6804ce6421ff58f
safi miazi <mohibullamiazi@gmail.com>
2025-03-05 12:23:26 +0600

    feat: add custom scrollbar styles and fix button class formatting in Top component

--- src/components/common/Top.tsx
+++ src/components/common/Top.tsx
@@ -6,7 +6,7 @@ const Top = ({ setIsMobileMenuOpen, isMobileMenuOpen }: any) => {
     <div className="h-16 border-b border-gray-200  flex items-center justify-between px-4 md:px-10 bg-white shadow-sm">
       <button
         type="button"
-        className="md:hidden z-50 p-2 mr-2 rounded-lg bg-white dark:bg-[#0F0F12] shadow-md"
+        className="md:hidden z-50 p-2  mr-2 rounded-lg bg-white dark:bg-[#0F0F12] shadow-md"
         onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
       >
         <MenuIcon className="h-5 w-5 text-gray-600 dark:text-gray-300" />
