df1ea2dc4eb395e72ddc630c711fb9d85a765f99
safi miazi <mohibullamiazi@gmail.com>
2025-03-07 12:33:55 +0600

    fix: update z-index in ShopingBag component for improved layering

--- src/components/ui/ShopingBag.tsx
+++ src/components/ui/ShopingBag.tsx
@@ -1,7 +1,7 @@
 import { ShoppingCart } from "lucide-react";
 
 const ShopingBag = () => (
-  <div className="fixed z-[2000] right-4 top-1/2 -translate-y-1/2 bg-blue-500 text-white rounded-lg shadow-lg p-2 w-20 flex flex-col items-center">
+  <div className="fixed z-[700] right-4 top-1/2 -translate-y-1/2 bg-blue-500 text-white rounded-lg shadow-lg p-2 w-20 flex flex-col items-center">
     <div className="bg-white p-1.5 rounded-full shadow-md hover:rotate-6 transition-transform">
       <ShoppingCart size={24} className="text-blue-600" />
     </div>
