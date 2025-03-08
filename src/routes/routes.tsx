eba0f38d35ffe61ccda1ad39023cb1c33962fd44
safi miazi <mohibullamiazi@gmail.com>
2025-03-08 22:38:37 +0600

    feat: add LoginRegistration component and update routing for authentication

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,27 +5,32 @@ import ProductDetails from "../pages/user/ProductDetails/ProductDetails";
 import Admin from "../Admin";
 import { routeGenerator } from "../utils/routesGenerator";
 import { adminPaths } from "./admin.routes";
+import LoginRegistration from "../pages/auth/LoginRegistration";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        children: [
-            {
-                index: true,
-                element: <Home />, 
-            },
-            {
-                path: "/details",
-                element: <ProductDetails />, 
-            }
-        ]
-    },
-    {
-        path: "/admin",
-        element: <Admin/>,
-        children: routeGenerator(adminPaths)
-    },
-])
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/details",
+        element: <ProductDetails />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+    children: routeGenerator(adminPaths),
+  },
+  {
+    path: "/auth",
+    element: <LoginRegistration />,
+  },
+]);
 
 export default router;
