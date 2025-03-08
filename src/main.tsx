b7969ca43f4c1437318e2230b2a320b1a4a1eebb
safi miazi <mohibullamiazi@gmail.com>
2025-03-08 15:03:33 +0600

    feat: integrate Redux for state management with react-redux setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import { ConfigProvider } from "antd";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -19,7 +21,9 @@ createRoot(document.getElementById("root")!).render(
         },
       }}
     >
-      <RouterProvider router={router}></RouterProvider>
+      <Provider store={store}>
+        <RouterProvider router={router}></RouterProvider>
+      </Provider>
     </ConfigProvider>
   </StrictMode>
 );
