9678dfbe6e1eac7ef8032f9f239893a06197022d
safi miazi <mohibullamiazi@gmail.com>
2025-03-12 16:46:37 +0600

    refactor: clean up imports in Products component

--- src/pages/admin/products/Products.tsx
+++ src/pages/admin/products/Products.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from "react";
+import  { useState } from "react";
 import {
   Button,
   Drawer,
@@ -8,7 +8,6 @@ import {
   InputNumber,
   Upload,
   Select,
-  message,
   Popconfirm,
   notification,
 } from "antd";
