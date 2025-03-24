e720ee9c796a10cce8336e070844c723db5bdb1a
safi miazi <mohibullamiazi@gmail.com>
2025-03-24 15:00:29 +0600

    feat: implement wishlist API endpoints for CRUD operations

---
+++ src/redux/api/wishlistApi/WishlistApi.tsx
@@ -0,0 +1,69 @@
+import { baseApi } from "../baseApi";
+
+const wishlistApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    wishlistPost: build.mutation({
+      query: (data) => {
+        return {
+          url: "/wishlist/create",
+          method: "POST",
+          body: data,
+        };
+      },
+    }),
+    wishlistUpdate: build.mutation({
+      query: ({ data, id }) => {
+        return {
+          url: `/wishlist/update/${id}`,
+          method: "PUT",
+          body: data,
+        };
+      },
+    }),
+
+    wishlistDelete: build.mutation({
+      query: ({ id }) => ({
+        url: `/wishlist/${id}`,
+        method: "DELETE",
+      }),
+    }),
+    bulkDelete: build.mutation({
+      query: ({ ids }) => ({
+        url: `/wishlist/bulk`,
+        method: "POST",
+        body: { ids },
+      }),
+    }),
+    getwishlistData: build.query({
+      query: ({ pageIndex, pageSize, search, isDelete }) => ({
+        url: "/wishlist",
+        method: "GET",
+        params: {
+          limit: pageSize,
+          page: pageIndex,
+          searchTerm: search,
+          isDelete,
+        },
+      }),
+    }),
+
+    getSinglewishlistData: build.query({
+      query: ({ id }) => ({
+        url: `/wishlist/get_wishlist/${id}`,
+        method: "GET",
+      }),
+    }),
+  }),
+
+  overrideExisting: false,
+});
+
+export const {
+    useWishlistDeleteMutation,
+    useWishlistPostMutation,
+    useWishlistUpdateMutation,
+    useGetwishlistDataQuery,
+    useBulkDeleteMutation,
+    useGetSinglewishlistDataQuery,
+
+} = wishlistApi;
